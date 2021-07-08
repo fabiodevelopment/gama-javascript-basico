553154fd5a8b5c9b5a308e4998051155b34c64d8
Fabio Haddad <fabiohpaes@gmail.com>
2021-07-08 20:46:52 -0300

    [aula02] Tipos de dados

---
+++ script-tipos-de-dados.js
@@ -0,0 +1,35 @@
+var myName = "Fabio" //string
+
+var age = 34 //number
+
+var height = 1.65 //number
+
+null
+undefined
+
+var isStudent = false //boolean
+
+// Object, array e function
+
+var person = {
+    "name"  :   "Simara",
+    "age"   :   32
+}
+
+var students = ["Fabio", "Jonny", "Bob"];
+
+function sum(a,b) {
+    return a + b
+}
+
+console.log(sum(10,3));
+
+//Operador typeOf
+var isStudent = true
+var objeto = null
+console.log("person", typeof person)
+console.log("students", typeof students)
+console.log("objeto", typeof objeto)
+console.log("sum", typeof sum)
+console.log("32", typeof 32)
+console.log("isStudent", typeof isStudent)
