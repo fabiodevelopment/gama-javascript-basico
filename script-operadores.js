41c3d11a1a0d8d6a057b06980f6f458b4c30b1af
Fabio Haddad <fabiohpaes@gmail.com>
2021-07-08 21:05:19 -0300

    [aula03] Operadores

---
+++ script-operadores.js
@@ -0,0 +1,57 @@
+// Operadores aritméticos
+
+// Adição +
+var sumTwoNumbers = 2 + 2;
+console.log('sumTwoNumbers', sumTwoNumbers);
+
+// Subtração -
+var subTwoNumbers = 10 - 5;
+console.log('subTwoNumbers', subTwoNumbers);
+
+// Multiplicação *
+var multTwoNumbers = 10 * 5;
+console.log('multTwoNumbers', multTwoNumbers);
+
+// Divisão /
+var divtTwoNumbers = 50 / 5;
+console.log('divtTwoNumbers', divtTwoNumbers);
+
+// Módulo ou resto de uma divisão %
+var restTwoNumbers = 8 % 3;
+console.log('restTwoNumbers', restTwoNumbers)
+
+// Incremento ++
+var num = 1;
+// console.log('num', num++) //1
+console.log('++num', ++num) // num = num + 1
+
+// Decremento --
+console.log('--num', --num)
+
+// Operador de Atribuição
+// = Atribuição
+
+var x = 1
+var y = 3
+
+console.log( x+=y ) //x+=y == x = x+y
+console.log( x-=y )
+console.log( x*=y )
+console.log( x/=y )
+console.log( x%=y )
+
+//Operadores de comparação
+
+// ==
+// ===
+// !=
+// < ou <=
+// > ou >=
+
+//Operadores Lógicos
+
+// e &&
+// ou ||
+// negação ! 
+
+
