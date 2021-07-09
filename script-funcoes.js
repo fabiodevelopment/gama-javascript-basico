afe2f2af06e1bf522142bb70e63b09e788ff1f33
Fabio Haddad <fabiohpaes@gmail.com>
2021-07-08 23:11:22 -0300

    [aula06] Funções

---
+++ script-funcoes.js
@@ -0,0 +1,13 @@
+function sayHello(name, lastName) {
+    console.log( 'Hello ' + name + ' ' + lastName );
+}
+
+sayHello("Fabio", "Haddad Paes");
+
+function sum(a, b) {
+    return a + b;
+}
+
+console.log(sum(10,40));
+
+console.log(typeof sayHello);
